b30bda254b08d76b1f82339365d63b22b568d850
SIBAXO-0214 <y.27142228@gmail.com>
2026-02-11 19:59:49 +0900

    kadai_005をアップロードしました

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,9 +1,8 @@
-let weight='68';
+let weight=68;
 console.log(weight);
 
-let height='1.7';
+let height=1.7;
 console.log(height);
 
-console.log(68 / (1.7 * 1.7));
-
-let bmi='23.529411764705884';
+let bmi=(weight/(height*height));
+console.log(bmi);
